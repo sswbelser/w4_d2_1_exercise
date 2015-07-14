2d7f9f200d0a2318116dd400bd33f9e5a4161fc3
Sabastian <sswbelser@gmail.com>
2015-07-14 12:21:25 -0700

    Filled in comments

--- exercise/models.js
+++ exercise/models.js
@@ -1,7 +1,7 @@
 // Setup
 var mongoose = require("mongoose");
 
-// what does the next line do?
+// what does the next line do? Connects mongoose to the MongoDB
 mongoose.connect("mongodb://localhost/mongoRelationships");
 
 var Schema = mongoose.Schema;
@@ -9,19 +9,19 @@ var Schema = mongoose.Schema;
 
 // Referenced Data
 
-// what does this code block do?
+// what does this code block do? Declares variable's "name" and "ingredients" under the Schema
 var foodSchema = new Schema({
   name: {
     type: String,
     default: ""
   },
   ingredients: [{
-    type: Schema.Types.ObjectId,  // what does this line do?
+    type: Schema.Types.ObjectId,  // what does this line do? Sets a point of reference for the Ingredients
     ref: 'Ingredient'
   }]
 });
 
-// what does this code block do?
+// what does this code block do? Declares variable's "title" and "origin" under the Schema; and as referenced by foodSchema
 var ingredientSchema = new Schema({
   title: {
     type: String,
@@ -34,12 +34,12 @@ var ingredientSchema = new Schema({
 })
 
 
-// what do these next two lines do?
+// what do these next two lines do? Sets the main variables using mongoose; to be referenced in the server JS
 var Food = mongoose.model("Food", foodSchema);
 var Ingredient = mongoose.model("Ingredient", ingredientSchema);
 
 // Embedded Data
-// what does this code block do?
+// what does this code block do? Declares variable's "body" under the tweet Schema
 var tweetSchema = new Schema({
   body: {
     type: String,
@@ -47,16 +47,16 @@ var tweetSchema = new Schema({
   }
 });
 
-// what does this code block do?
+// what does this code block do? Declares variable's "name" and "tweets" under the user Schema
 var userSchema = new Schema({
   name: {
     type: String,
     default: ""
   },
-  tweets: [tweetSchema]   // what does this line do?
+  tweets: [tweetSchema]   // what does this line do? Embeds the tweetSchema under "tweets" to this userSchema variable
 });
 
-// what do these next two lines do?
+// what do these next two lines do? Sets the main variables using mongoose; to be referenced in the server JS
 var User = mongoose.model("User", userSchema);
 var Tweet = mongoose.model("Tweet", tweetSchema);
 
